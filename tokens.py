25acb8e648b7d35a7b695e4ac3963534a126a9a8
Wilson <72035077+CtrlAltWilson@users.noreply.github.com>
2022-04-13 15:12:41 -0500

    Create tokens.py

---
+++ tokens.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# This program is dedicated to the public domain under the CC0 license.
+
+"""
+TOKENS
+
+"""
+
+#RASRAS
+TOKEN = ""
+
+#RASPI
+TOKEN2 = ""
+
+#SpotifyCID
+SpotifyCID = ""
+
+#SpotifyCS
+SpotifyCS = ""
+
+#Smartthings = https://account.smartthings.com/tokens
+SmartThings = ''
+
+telegram_master = ""
+
+spotify_user = ""
